d07213721727920ad170a40959662382884733a0
codeevoyager <vinothfromearth@gmail.com>
2023-09-23 20:41:32 +0530

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer() {
+    return(
+        <div className='fixed-bottom w-100 text-bg-dark text-center fs-6'>Created by <a className='text-bg-dark' href="https://github.com/codeevoyager" target="_blank">codeevoyager.</a> Code on <a className='text-bg-dark' href="https://github.com/codeevoyager/employee-management-crud-react" target="_blank">Github</a>
+        </div>
+    )
+}
+
+export default Footer
