e195496eb6f7f0351ea7213c538ba08707838681
codeevoyager <vinothfromearth@gmail.com>
2024-02-08 10:25:02 +0530

    form layout has been modified for responsive

--- src/Components/Update.js
+++ src/Components/Update.js
@@ -34,26 +34,26 @@ function Update() {
 
   return (
     <Form className="form py-2 bg-muted">
-    <Form.Group className='py-3 w-25'>
+    <Form.Group className='py-3'>
       <Form.Label>Name of the Employee</Form.Label>
       <Form.Control type="text" value={empName} placeholder="Enter Name"
       onChange={e => setEmpname(e.target.value)} 
       />
     </Form.Group>
 
-    <Form.Group className='py-3 w-25'>
+    <Form.Group className='py-3'>
       <Form.Label>Designation</Form.Label>
       <Form.Control type="text" value={position} placeholder="Enter Designation"
       onChange={e => setPosition(e.target.value)}/>
     </Form.Group>
 
-    <Form.Group className='py-3 w-25'>
+    <Form.Group className='py-3'>
       <Form.Label>Employee ID</Form.Label>
       <Form.Control type="number" value={empId} placeholder="Enter Employee ID"
       onChange={e => setEmpid(e.target.value)}/>
     </Form.Group>
 
-    <Form.Group className='py-3 w-25'>
+    <Form.Group className='py-3'>
       <Form.Label>Mobile Number</Form.Label>
       <Form.Control type="number" value={mobNo} placeholder="Enter Mobile number"
       onChange={e => setMobno(e.target.value)} />
